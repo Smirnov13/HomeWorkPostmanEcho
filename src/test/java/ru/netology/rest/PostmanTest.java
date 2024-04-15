4aa158aa4ccd6021f10096428eaab51aa1c683ec
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-04-15 18:28:11 +0300

    Fail CI

---
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanTest {
+    @Test
+    void shouldReturnSendData() {
+//        String text = "Good job";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("text")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Text"));
+
+    }
+}
